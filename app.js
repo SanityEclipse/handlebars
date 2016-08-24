9f99cbb7b1d95d22aabc55cbcab59d131c0b26c7
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-08-24 11:37:41 -0600

    functional example of handlebars

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-var express = require ('express');
+var express = require('express');
+var hbs = require('hbs');
+var locals = require('./locals.js')
+
 var app = express();
 
-app.get('/', (req, res)=>{
-  res.json({message: 'Hello from express!'});
+app.set('view engine', 'hbs');
+hbs.localsAsTemplateData(app);
+
+locals(app);
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        cohortName: 'g31',
+        foods: [
+            'chalupa',
+            'ceviche',
+            'candy sprinkles'
+        ]
+    });
 });
 
-app.get('/page2', (req, res)=> {
-  res.json({message: 'I am page 2!!!'});
+app.get('/page2', (req, res) => {
+    res.render('page2');
 });
 
-app.listen(3000, function () {
-console.log('Listening on port 3000') ;
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
 });
---
+++ locals.js
@@ -0,0 +1,9 @@
+function locals(app) {
+
+    app.locals.cohortName = 'g31';
+    app.locals.title = 'My Handle Bars Example';
+
+}
+
+
+module.exports = locals;
